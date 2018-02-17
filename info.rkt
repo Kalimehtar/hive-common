ee66c7ca261c401b7f46c6df22577007615f4c86
Roman Klochkov <kalimehtar@mail.ru>
2018-02-17 15:16:44 +0500

    Fixed deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang setup/infotab
 (define version "0.50")
 (define collection 'multi)
-(define deps '("base" "scribble-lib"))
+(define deps '("base" "scribble-lib" "thread-utils"))
 (define build-deps '("racket-doc"))
